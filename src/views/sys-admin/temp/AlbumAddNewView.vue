608138b9dc548781a7278a8027d7d2c204da53d0
aswobb <zhubowen566@gmail.com>
2023-12-12 17:12:08 +0900

    日语翻译

--- src/views/sys-admin/temp/AlbumAddNewView.vue
+++ src/views/sys-admin/temp/AlbumAddNewView.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <h1>添加相册</h1>
+    <h1>アルバムを追加</h1>
     <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-      <el-form-item label="名称" prop="name">
+      <el-form-item label="名前" prop="name">
         <el-input v-model="ruleForm.name"></el-input>
       </el-form-item>
-      <el-form-item label="简介" prop="description">
+      <el-form-item label="紹介" prop="description">
         <el-input v-model="ruleForm.description"></el-input>
       </el-form-item>
-      <el-form-item label="排序序号" prop="sort">
+      <el-form-item label="並び順" prop="sort">
         <el-input v-model="ruleForm.sort"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">添加相册</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
+        <el-button type="primary" @click="submitForm('ruleForm')">アルバムを追加</el-button>
+        <el-button @click="resetForm('ruleForm')">リセット</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -30,16 +30,16 @@ export default {
       },
       rules: {
         name: [
-          {required: true, message: '请输入相册名称', trigger: 'blur'},
-          {min: 2, max: 20, message: '长度在 2 到 20 个字符', trigger: 'blur'}
+          { required: true, message: 'アルバム名を入力してください', trigger: 'blur' },
+          { min: 2, max: 20, message: '2文字から20文字までの長さを入力してください', trigger: 'blur' }
         ],
         description: [
-          {required: true, message: '请输入相册简介', trigger: 'blur'},
-          {min: 2, max: 30, message: '长度在 2 到 30 个字符', trigger: 'blur'}
+          { required: true, message: 'アルバム紹介を入力してください', trigger: 'blur' },
+          { min: 2, max: 30, message: '2文字から30文字までの長さを入力してください', trigger: 'blur' }
         ],
         sort: [
-          {required: true, message: '请输入相册排序', trigger: 'blur'},
-          {min: 1, max: 2, message: '长度在 1 到 2 个数字', trigger: 'blur'}
+          { required: true, message: 'アルバム並び順を入力してください', trigger: 'blur' },
+          { min: 1, max: 2, message: '1桁から2桁までの数字を入力してください', trigger: 'blur' }
         ]
       }
     };
@@ -50,32 +50,32 @@ export default {
         if (valid) {
           // alert('您输入的用户名和密码的格式是正确的，但是，登录功能我们还没做!');
           let url = 'http://localhost:9090/albums/add-new';
-          console.log('尝试添加相册')
-          console.log('请求路径为:' + url)
-          console.log('请求参数为:' + this.ruleForm)
+          console.log('アルバムを追加しようとしています')
+          console.log('リクエストパス:' + url)
+          console.log('リクエストパラメータ:' + this.ruleForm)
           console.log(this.ruleForm)
           let fromDate = this.qs.stringify(this.ruleForm)
-          console.log('将ruleForm转换为fromDate')
+          console.log('ruleFormをfromDateに変換')
           console.log(fromDate)
           this.axios.post(url, fromDate).then((response) => {
             console.log(response);
             let responseDate = response.data
             if (responseDate.state == 20000) {
-              console.log('添加相册成功');
+              console.log('アルバム追加成功');
               this.$message({
-                message: '添加相册成功',
+                message: 'アルバム追加成功',
                 type: 'success'
               });
             } else {
               console.log(responseDate.message)
               this.$notify.error({
-                title: '登录失败',
+                title: '失敗しました',
                 message: responseDate.message
               });
             }
           });
         } else {
-          console.log('error submit!!')
+          console.log('エラーが発生しました')
           return false;
         }
       });
