8dfb5e2796d02e8081434d8ceb1f3aff39649ad3
DONG ZHENGBO <chunghwa1997@gmail.com>
2023-12-12 16:34:01 +0900

    プロジェクトにVuetifyを導入しました。

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
+
+const opts = {}; // 在这里设置 Vuetify 选项
+
+export default new Vuetify(opts);
